dd1941ac6ba9a0760feca387c6c747f427779d30
matt@gigabug.org <bowrango@gmail.com>
2025-12-24 01:16:35 -0500

    cleanup

--- co/check_lang_kernel.m
+++ co/check_lang_kernel.m
@@ -5,6 +5,7 @@ n = 3;
 N = 2^n;
 
 % PSD
+rng default
 Q = randn(n);
 Q = Q + Q';
 
@@ -13,25 +14,24 @@ lr = 0.1;
 
 P = kernel(Q, temp, lr);
 mc = dtmc(P);
-probm = asymptotics(mc);
+[probm, tMix] = asymptotics(mc);
 
+% X in {-1,+1}
 X = 2*(dec2bin(0:N-1, n) - '0') - 1;
 
 spin2idx = @(s) bin2dec( num2str((s'+1)/2, '%1d') ) + 1;
 
-K = 1e6;
+K = 1e5;
 
-rng default
-x = X(randi(N), :)';
+s = X(randi(N), :)';
 counts = zeros(1,N);
 for k = 1:K
     % Vanilla LD (DULA)
-    g = dH(x, Q);
-    x = x - (lr/temp)*g + sqrt(2*lr)*randn(n,1);
-    x = sign(x);
-    x(x == 0) = 1;
+    g = dH(s, Q);
+    s = sign(s - (lr/temp)*g + sqrt(2*lr)*randn(n,1));
+    s(s == 0) = 1;
 
-    idx = spin2idx(x);
+    idx = spin2idx(s);
     counts(idx) = counts(idx) + 1;
 end
 probl = counts / sum(counts);
@@ -48,21 +48,14 @@ function P = kernel(Q, temp, lr)
     P = zeros(N,N);
     sigma = sqrt(2*lr);
     for i = 1:N
-        x = X(i,:)';          % current spin config (column)
-        g = dH(x, Q);         % gradient ∇H(x)
-        mu = x - (lr/temp)*g; % mean of Gaussian proposal y
-
-        % Componentwise probabilities P(x'_k = +1 | x)
-        p_plus = normcdf(mu / sigma);   % n×1 vector
-
+        x = X(i,:)';
+        g = dH(x, Q);
+        mu = x - (lr/temp)*g;
+        % componentwise probabilities
+        p_plus = normcdf(mu / sigma);
         for j = 1:N
-            s = X(j,:)';      % candidate next spin config
-
-            % For each bit k:
-            % if s_k = +1 → use p_plus(k)
-            % if s_k = -1 → use 1 - p_plus(k)
-            prob = prod( p_plus(s==1) ) * prod( (1 - p_plus(s==-1)) );
-
+            s = X(j,:)';
+            prob = prod(p_plus(s==1)) * prod((1 - p_plus(s==-1)));
             P(i,j) = prob;
         end
     end
--- co/langevin.m
+++ co/langevin.m
@@ -3,91 +3,123 @@
 % [1] [Fractional Langevin Dynamics for Combinatorial Optimization via Polynomial-Time Escape](https://openreview.net/pdf?id=BZ0igCEeoU)
 % [2] [Regularized Langevin Dynamics for Combinatorial Optimization](https://arxiv.org/pdf/2502.00277)
 
-N = 4;
+sigmoid = @(x) reshape(1 ./ (1 + exp(-x)), size(x));
+kldiv = @(p,q) sum(p.*log2(p./q), 'omitnan');
+
+n = 3;
+N = 2^n;
 
 rng default
 
 % convert max-cut graph to qubo
-A = randn(N);
-A = (A+A')/2;
-A(1:N+1:end) = 0;
-qb = maxcut2qubo(graph(A));
-
-% exact = solve(qb);
-
+% A = randn(N);
+% A = (A+A')/2;
+% A(1:N+1:end) = 0;
+% qb = maxcut2qubo(graph(A));
 % build Q matrix (not PSD!)
-Q = qb.QuadraticTerm;
-Q(1:N+1:end) = qb.LinearTerm;
-
+% Q = qb.QuadraticTerm;
+% Q(1:N+1:end) = qb.LinearTerm;
 % H(exact.BestX, Q) == exact.BestFunctionValue
 
-sigmoid = @(x) 1 / (1 + exp(-x));
+% Indefinite
+% Q = randn(n);
+% Q = tril(Q,-1) + triu(Q',1);
 
-dt = 0.01;
-T = 100;
-Nsteps = T / dt;
+% PSD
+Q = randn(n);
+Q = Q + Q';
+
+% Boltzmann
+X = (2*(dec2bin(0:N-1, n)=='1') - 1)';
+Eb = -(sum(X.*(Q*X), 1)/2);
+
+T = 50;
+K = 1e3;
 
 % temperature schedule
-tau = @(t) 0.01 + (1 - t/T);
+% tau = @(t) 0.01 + (1 - t/T);
+tau0 = 5;
+tau = @(t) tau0/t;
+% learning rate schedule
+eta = @(t) 1/t;
 
-sz = [N 1];
-x = randi([0 1], sz); % {0,1}
-h = x; % relaxation [0 1]
-E = zeros(1,T);
+alpha = 2;
+c_alpha = 0.5;
+
+sz = [n 1];
+% x in {0,1}
+x = randi([0 1], sz);
+x2 = x;
+% relaxation h in [0 1]
+h1 = x; % Gaussian
+h2 = x; % Fractional
+Et = zeros(2,T);
 for t = 1:T
+
     temp = tau(t);
-
-    % pg = sigmoid( -dH(x,Q)/temp ).';
-    % Ig = double(rand(sz) < pg); % = binornd(?,pg);
-    % pz = sigmoid( -dH(x,Q)/temp ).';
-    % Iz = double(rand(sz) < pz);
-    % grad_iter = dt*(-1/temp)*dH(x,Q);
-    % z_iter = sqrt(dt)*randn(sz);
-    % update
-    % h = h - Ig.*grad_iter + Iz.*z_iter;
-
-    drift = -dH(h, Q)/temp;
-    noise = sqrt(2*dt)*randn(sz);
+    lr = eta(t);
     
-    h = h + drift*dt + noise;
-    h = clip(h, 0, 1);
+    % TODO
+    % Pt = kernel(Q, temp, lr);
+    % mc = dtmc(Pt);
+    % [probm, tMix] = asymptotics(mc);
+    
+    counts = zeros(1, N);
+    for k = 1:K
+        
+        % Vanilla LD
+        g1 = dH(2*h1-1, Q);
+        h1 = h1 - lr*g1/temp + sqrt(2*lr)*randn(sz);
+        % h1 = clip(h1, 0, 1);
+        x1 = double(rand(sz) < h1);
+        idx = bin2dec(join(string(x1'),""))+1;
+        counts(idx) = counts(idx)+1;
+    
+        % Fractional LD
+        g2 = dH(x2, Q);
+        
+        pg = sigmoid( (0.5*(2*x2 - 1).*g2)/temp );
+        Ig = double(rand(sz) < pg); % = binornd(?,pg);
+        pz = sigmoid( -(0.5*(2*x2 - 1).*g2)/temp );
+        Iz = double(rand(sz) < pz);
 
-    x = double(rand(sz) < h);
-    E(t) = H(x,Q);
+        grad_iter = lr*c_alpha*g2/temp;
+        z_iter = lr^(1/alpha)*levy(alpha,sz);
+
+        h2 = h2 - Ig.*grad_iter + Iz.*z_iter;
+        h2 = clip(h2, 0, 1);
+        x2 = double(rand(sz) < h2);
+    end
+    probl = counts/sum(counts);
+    
+    Et(1,t) = H(2*x1-1,Q);
+    Et(2,t) = H(2*x2-1,Q);
 end
 
-plot(1:T, E)
+% expE = exp(-Eb./temp);
+% probb = expE/sum(expE);
 
-
-% drift/score and diffusion
-% F = @(t, x) -dH(x,Q)/tau(t);
-% F = @(t, z) drift(t, z, Q, tau);
-% G = @(t, x) sqrt(2).*eye(N);
-% 
-% model = sde(F, G, StartState=x);
-% [U, T] = simulate(model, Nsteps);
-% 
-% Z = sigmoid(U);
-% 
-% X = Z > rand(size(Z));
-% 
-% plot(T, X(:,1))
+figure
+% subplot(2,1,1)
+hold on
+plot(1:T, Et, 'o')
+legend('Vanilla', 'Fractional')
+yline(min(Eb))
+grid on
 
 
 
+
+function z = levy(alpha, sz)
+w = pi*rand(sz) - pi/2;
+u = rand(sz);
+z = (sin(alpha.*w) ./ cos(w).^(1/alpha)) .* (cos((1-alpha).*w) ./ -log(u)).^((1-alpha)/alpha);
+end
 function e = H(x,Q)
-    e = x'*Q*x;
+    e = -(x'*Q*x)/2;
 end
 function e = dH(x,Q)
-    e = 2*Q*x;
+    e = -Q*x;
 end
 
-% function du = drift(t, h, Q, tau)
-%     % Drift in logit-space
-%     dsigmoid = @(h) h .* (1 - h);
-% 
-%     h = (1 / (1 + exp(-h))).';
-%     x = h;
-%     du = -(dH(x,Q) .* dsigmoid(h))/tau(t);
-% end
 
